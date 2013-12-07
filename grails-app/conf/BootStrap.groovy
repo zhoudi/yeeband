c8f225be9fa765c91288d6869f7358fe552df522
judi.zhoud <zhoudi77777@gmail.com>
2013-12-07 21:40:59 +0800

    加入spring配置,加入初始化两个用户

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,26 @@
+import yeeband.Role
+import yeeband.User
+import yeeband.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        if (User.count() == 0 && Role.count() == 0) {
+            //init data
+            def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+            def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+            def initUser = new User(username: 'user', enabled: true, password: 'user', email: 'user@yeeband.com')
+            initUser.save(flush: true)
+
+            def initAdmin = new User(username: 'admin', enabled: true, password: 'admin', email: 'admin@yeeband.com')
+            initAdmin.save(flush: true)
+
+            UserRole.create initUser, userRole, true
+            UserRole.create initAdmin, adminRole, true
+        }
+
     }
     def destroy = {
     }
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,4 @@
 // Place your Spring DSL code here
 beans = {
+    importBeans('file:grails-app/conf/spring/*.xml')
 }
